a21bcd1cc8aa5219cb58c1f0e79d80666b746e6b
ggggggggggggg111 <107188601+yohan98123@users.noreply.github.com>
2022-06-14 21:57:00 +0900

    C# variables
    변수 여러가지 배움

--- FirstPoject/FirstPoject/Program.cs
+++ FirstPoject/FirstPoject/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 파란글자 : 키워드
+// 키워드(예약어)란 ? 미리 정의 되어있는 단어
+// 이미 문법용도로 사용 되고 있기 때문에 식별용으로 개발자가 쓸 수 없다.
+//
+// 흰글자 : 
+// 식별문자 (참조 있음, 명시적 표현)
+//
+// 청록색글자 : 
+// 클래스 타입 식별문자(이름)
+//
+// 노란색글자 : 
+// 함수 식별문자(이름)
+//
+// 하늘색글자 : 
+// 함수의 파라미터 (매개변수) 의 식별문자(이름)
+//
+// 주황색글자 : 
+// 문자/ 문자열 상수
+// 
+// 글자색이 어둡다면 : 
+// (참조 없음 / 생략 가능 == 암시적 표현)
+
+// using 키워드
+// 특정 namespace 를 사용하기 위한 키워드
+// 형식 : using namespace이름
+using System;
+
+// namespace 키워드
+// 공간을 구분하기위한 키워드 
+// (내부식별자를 가지고 namespace로 묶인 변수, 함수, 클래스, 인터페이스 등등을 구분함)
+namespace FirstProject // Note: actual namespace depends on the project name.
+{
+    // internal 키워드
+    // 동일 어셈블리에서만 접근 가능한 키워드
+
+    // class 키워드
+    // 객체를 만들기위한 타입을 정의하는 키워드
+    // 형식 : class 클래스이름
+    internal class Program
+    {
+         // static 키워드
+         // 정적 키워드, 메모리에 동적할당이 불가능한 속성을 부여함.
+          
+         // void 키워드
+         // 아무것도 없음. (반환값이 없음)
+
+         // Main 함수
+         // 실행파일 (.exe) 을 실행했을때 가장 먼저 실행되는 함수
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FirstPoject/Variables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Variables
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".");
+            Console.WriteLine(Human.age);
+            
+        }
+    }
+
+    class Human
+    {
+        // : 해당 라인을 주석처리함
+        /* */  // : 범위 내의 내용을 주석처리함
+
+        // bit : 한자리 이진수. (0 과 1, 정보 처리의 최소단위)
+        // byte : 8 bit 로 구성 ( CPU 의 데이터 처리의 최소단위)
+        // 4byte : 4 * 8 bit == 32bit
+        // 표현할 수 있는 숫자범위 :  2^(bit 수)
+
+        // (멤버)변수 선언
+        // 형식 : 자료형 변수이름
+        public static int age; // 4 byte 정수 -2^31 ~ 2^31 - 1
+        private float height; // 4 byte 실수형
+        double weight; // 8 byte 실수형
+        bool isResting; // 1 byte 논리형 { 참과 거짓 표현 }
+        // 참 : 0이 아닌 숫자, 거짓 : 0
+        char genderChar; // 2 byte 문자형 ( ASCII 코드로 표현 )
+        string name; // 문자열형. 문자갯수 * 2byte + 1 byte ( null )
+
+        // 멤버
+        // class / structure / namespace  를 구성하는 구성원을 멤버라 한다.
+        
+    }
+}
