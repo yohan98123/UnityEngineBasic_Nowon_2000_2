596877a66a80f19165d409f5c6e9ad03d9a2a2ce
ggggggggggggg111 <107188601+yohan98123@users.noreply.github.com>
2022-06-27 21:56:40 +0900

    C# 동적배열
    C# 동적배열 프로퍼티를 배웠다.

--- FirstPoject/Example03_DynamicArray/DynamicArray.cs
+++ FirstPoject/Example03_DynamicArray/DynamicArray.cs
@@ -12,14 +12,36 @@ namespace Example03_DynamicArray
         private const int DEFAULT_SIZE = 1;
         private int[] _data = new int[DEFAULT_SIZE];
 
-        public int Length; //데이터 갯수
-        public int Capacity; // 데이터 용량 
+        public int this[int index]
+        {
+            get
+            {
+                return _data[index];
+            }
+
+            set
+            {
+                _data[index] = value;
+            }
+        }
+        public int Length; //실제 데이터 갯수
+
+        // 프로퍼티 : 필드의 값을 쓰거나 읽을때 get 함수나 set 함수를 용이하게 만들어서 접근할 수 있는 
+        // get 접근자와 set 접근자를 구현할 수 있는 멤버
+        public int Capacity //_data 의 데이터 용량(길이) 
+        {            
+            get
+            {
+                return _data.Length;
+            }
+        }
+        
 
         public void Add(int item)
         {
             // Capacity 가 모자라면 배열 크기 늘림
             if (Length >= Capacity)
-            {
+            {                
                 int[] tmp = new int[Capacity * 2];
                 for (int i = 0; i < Length; i++)
                 {
@@ -29,6 +51,32 @@ namespace Example03_DynamicArray
             }
             _data[Length] = item;
             Length++;
+        }
+
+
+        public bool Remove(int item)
+        {
+            bool isFounded = false;
+            for (int i = 0; i < Length; i++)
+            {
+                if (_data[i] == item)
+
+                isFounded = true;
+                RemoveAt(i);
+                break;
+            }
+            return isFounded;
+        }
+        
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Length - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Length - 1] = default(int);
+            Length--;
         }
     }
 }
--- FirstPoject/Example03_DynamicArray/Program.cs
+++ FirstPoject/Example03_DynamicArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 //동적배열
 
@@ -8,7 +9,25 @@ namespace Example03_DynamicArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DynamicArray dynamicArray = new DynamicArray();
+            dynamicArray.Add(5);
+            dynamicArray.Add(4);
+            dynamicArray.Add(3);
+            dynamicArray.Add(2);
+            dynamicArray.Add(1);
+            Console.WriteLine(dynamicArray.Length);
+            Console.WriteLine(dynamicArray.Capacity);
+            Console.WriteLine(dynamicArray[3]);
+            dynamicArray[3] = 10;
+            Console.WriteLine(dynamicArray[3]);
+
+            List<int> list = new List<int>();
+            list.Add(5);
+            list.Remove(5);
+            list.RemoveAt(0);
+
+
+
         }
     }
 }
