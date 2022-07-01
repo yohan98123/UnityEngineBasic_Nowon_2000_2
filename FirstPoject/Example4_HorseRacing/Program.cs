2dc3b8b7d7237de76c4fc2d7f073b19e691af6a1
ggggggggggggg111 <107188601+yohan98123@users.noreply.github.com>
2022-07-01 21:48:56 +0900

    C# 구조체, 클래스심화
    구조체와 클래스를(가비지 컬렉션) 심화하여 배웠다.

--- FirstPoject/Example4_HorseRacing/Program.cs
+++ FirstPoject/Example4_HorseRacing/Program.cs
@@ -21,59 +21,78 @@ using System.Threading;
 //While 반복문에서 Thread.Sleep(1000); 을 추가하면 1초에 한번씩 반복문을 실행함.
 namespace Example4_HorseRacing
 {
-    internal class Program
+    class Program
     {
-        static Random random;
-        static bool isGameFinished = false;
-        static int minspeed = 10;
-        static int maxspeed = 20;
-        static int finishDistance = 200;
+        static Random random; // 말의 달리는 속도를 랜덤하게 생성하기위한 난수 변수
+        static bool isGameFinished = false; // 경주가 끝났는지 체크하는 변수
+        static int minSpeed = 10; // 달리는 속도의 최솟값
+        static int maxSpeed = 20; // 달리는 속도의 최댓값
+        static int finishDistance = 200; //  결승점 거리
 
         static void Main(string[] args)
-        {        
-            Horse[] arr_horse = new Horse[5];
-            string[] arr_FinishedHorseName = new string[5]; // 결승점을 통과할 말의 이름을 저장한 배열
-            int currentGrade = 1; //현재 등수
-            int lenth = arr_horse.Length;
-            for (int i = 0; i < lenth; i++)
+        {
+            Horse[] arr_Horse = new Horse[5]; // 말 5마리를 인스턴스화시키기 위한 배열
+            string[] arr_FinishedHorseName = new string[5]; // 결승점을 통과한 말의 이름을 저장할 배열
+            int currentGrade = 1; // 현재 등수
+
+            int length = arr_Horse.Length; // 말들이 저장된 배열의 길이
+            for (int i = 0; i < length; i++) // 말들을 인스턴스화 시키고, 말들의 이름을 초기화 하는 for 문
             {
-                arr_horse[i] = new Horse();
-                arr_horse[i].name = $"경주마{i+1}";
+                arr_Horse[i] = new Horse(); // 말 인스턴스화
+                arr_Horse[i].name = $"경주마{i + 1}"; // 말의 이름 초기화
             }
-            Console.WriteLine("경주시작!");
+            Console.WriteLine("경주 시작!");
             int count = 0;
-            
-            //경주중
             while (isGameFinished == false)
             {
-                Console.WriteLine($"======================={count}초=======================");
+                Console.WriteLine($"===================== {count} 초 ====================");
                 for (int i = 0; i < length; i++)
                 {
-                    if (arr_horse[i].dontMove == false)
+                    if (arr_Horse[i].dontMove == false) 
                     {
                         random = new Random();
-                        int tmpMoveDistance = random.Next(minspeed, maxspeed + 1);
-                        arr_horse[i].Run(tmpMoveDistance);
-
+                        int tmpMoveDistance = random.Next(minSpeed, maxSpeed + 1);
+                        arr_Horse[i].Run(tmpMoveDistance);
+                        Console.WriteLine($"{arr_Horse[i].name} 가 달린거리 : {arr_Horse[i].distance}");
+                        if (arr_Horse[i].distance >= finishDistance)
+                        {
+                            arr_Horse[i].dontMove = true;
+                            arr_FinishedHorseName[currentGrade - 1] = arr_Horse[i].name;
+                            currentGrade++;
+                        }
                     }
                 }
-                Thread.Sleep(1000);
+                Console.WriteLine($"====================================================");
+                if (currentGrade > 5)
+                {
+                    isGameFinished = true;
+                    Console.WriteLine("경주 끝!");
+                    break;
+                }
 
+                Thread.Sleep(1000);
+                count++;
+            }
+
+            Console.WriteLine("==============결과 발표==============");
+            for (int i = 0; i < length; i++) // 경주가 끝난 말의 이름을 등수대로 출력해주는 for 문
+            {
+                Console.WriteLine($"{i + 1} 등 : {arr_FinishedHorseName[i]}");
             }
         }
-    }
 
-    class Horse
+        class Horse
 
-    {
-        public string name;
-        public int distance;
-        public bool dontMove;
-        public void Run(int movedistance)
         {
-            distance += movedistance;
-        }
-            
+            public string name;
+            public int distance;
+            public bool dontMove;
+            public void Run(int movedistance)
+            {
+                distance += movedistance;
+            }
 
+
+        }
     }
 }
---
+++ FirstPoject/Structure/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+// Structure : 구조체 
+// 변수와 함수들을 멤버로 가지는 사용자정의 자료형
+public struct Stats
+{
+    public int STR;
+    public int DEX;
+    public int INT;
+    public int LUK;
+
+    public Stats(int STR, int DEX, int INT, int LUK)
+    {
+        this.STR = STR;
+        this.DEX = DEX;
+        this.INT = INT;
+        this.LUK = LUK;
+    }
+
+    public int GetCombatPower()
+    {
+        return STR + DEX + INT + LUK;
+    }
+
+
+}
+
+public class Stats_class
+{
+    public int STR;
+    public int DEX;
+    public int INT;
+    public int LUK;
+
+    public Stats_class(int STR, int DEX, int INT, int LUK)
+    {
+        this.STR = STR;
+        this.DEX = DEX;
+        this.INT = INT;
+        this.LUK = LUK;
+    }
+
+    public int GetCombatPower()
+    {
+        return STR + DEX + INT + LUK;
+    }
+}
+
+
+// 구조체 vs 클래스
+// 한번에 값을 복사할 수 있는 크기가 16Byte (.Net 버전에따라 상이할수있음)
+// 기본적으로 값형식에다가 바로 값을 할당하는게 참조형식에 값을 할당하는것 보다 빠르지만
+// 대입해야하는 객체의 크기가 16Byte 를 넘어서게 되면 2번 이상에 걸쳐서 값을 복사해야함. 
+// 2번 값을 복사하는 과정이 참조형식을 복사해서 수정하는것 보다 느림.
+// 따라서 16 Byte를 초과하는 구조에 대해서는 모두 클래스로 정의하는것을 권장함.
+// 16Byte 이하이면서 값의 복사가 빈번한경우 반드시 구조체로 정의해아함.
+
+
+namespace Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# 에서 구조체는 값형식 
+            // 
+            Stats stats1 = new Stats(10, 30, 50, 20);
+            Stats stats2;
+            stats2.STR = 0;
+            stats2.DEX = 0;
+            stats2.INT = 0;
+            stats2.LUK = 0;
+            Stats_class stats3 = new Stats_class(40, 20 ,20 ,10);
+
+            IsEnterPossible(stats1);
+            IsEnterPossible(stats3);
+
+            Console.WriteLine(stats1.STR);
+            Console.WriteLine(stats3.STR);
+        }
+
+        static bool IsEnterPossible(Stats stats)
+        {
+            stats.STR = 100;
+            if (stats.GetCombatPower() > 100)
+                return true;
+            return false;
+
+        }
+        
+        // 구조체는 값형식이므로 인자로 전달한 값이 파라미터로 대입됨.
+        // 파라미터를 수정하면 원래 인자가 참조하는 개체가 수정됨.(같은 객체를 가리키고 있다.)
+
+        static bool IsEnterPossible(Stats_class stats)
+        {
+            stats.STR = 999;
+            if (stats.GetCombatPower() > 100)
+                return true;
+            return false;
+        }
+
+    }
+}
