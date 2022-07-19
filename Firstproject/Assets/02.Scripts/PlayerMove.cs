7da47700dcc1eae60c378c8413716feb3b23b4d2
yohan98123 <107188601+yohan98123@users.noreply.github.com>
2022-07-19 21:48:45 +0900

    UnityGameEngine
    Learned UnityGameEngine - Camera,source

---
+++ Firstproject/Assets/02.Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float movespeed = 2.0f;
+    public float rotatespeed;
+
+    private void FixedUpdate()
+    {
+       float h = Input.GetAxis("Horizontal"); // x 축 입력
+       float v = Input.GetAxis("Vertical"); // z 축 입력
+
+        Vector3 dir = new Vector3(h, 0, v).normalized;
+        Vector3 moveVec = dir * movespeed * Time.fixedDeltaTime;
+        transform.Translate(moveVec);
+
+        float r = Input.GetAxis("Mouse X");
+        Vector3 rotateVec = new Vector3(0, r, 0) * rotatespeed * Time.fixedDeltaTime;
+        transform.Rotate(rotateVec);
+    }
+
+}
